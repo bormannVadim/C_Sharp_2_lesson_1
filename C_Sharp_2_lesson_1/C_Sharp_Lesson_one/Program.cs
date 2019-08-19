b1561c07314223659483b5e65dab8b66a7b23831
bormannVadim <freedom-mv47@rambler.ru>
2019-08-19 23:17:56 +0300

    ветка учителя

--- C_Sharp_2_lesson_1/C_Sharp_Lesson_one/Program.cs
+++ C_Sharp_2_lesson_1/C_Sharp_Lesson_one/Program.cs
@@ -1,8 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
 using System.Windows.Forms;
 
 namespace C_sharp_lesson_one
